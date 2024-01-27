1f1c4ce2e179bf31bba63835cbd3bf8010f33ab3
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-01-26 21:05:00 -0600

    Add ui to rps game

--- javaScript.js
+++ javaScript.js
@@ -1,119 +1,95 @@
-// Simulate a best of 5 match
-function game(rounds) {
+document.addEventListener('DOMContentLoaded', function() {
+
     let humanWins = 0;
-    for(let i = 0; i < rounds; i++) {
-        // Get selection from player
-        let playerSelection = null
-        do {
-        playerSelection = prompt("Rock, Paper, or Scissors?").toUpperCase();
-        } while (playerSelection.toUpperCase() != "ROCK" && playerSelection.toUpperCase() != "PAPER" && playerSelection.toUpperCase() != "SCISSORS");
+    const totalRounds = 5;
+    let currentRound = 0;
 
-        // Get selection from computer
-        const computerSelection = getComputerChoice();
+    const results = document.getElementById("results");
+    const currentScore = document.getElementById("currentScore");
 
-        console.log(computerSelection);
-        
-        // Play round and track number of wins
-        humanWins = humanWins + playRound(playerSelection, computerSelection);
+    const btnRock = document.getElementById("btnRock");
+    btnRock.addEventListener("click", () => {
+        playRound("ROCK", getComputerChoice());
+    });
+
+    const btnPaper = document.getElementById("btnPaper");
+    btnPaper.addEventListener("click", () => {
+        playRound("PAPER", getComputerChoice());
+    });
+
+    const btnScissors = document.getElementById("btnScissors");
+    btnScissors.addEventListener("click", () => {
+        playRound("SCISSORS", getComputerChoice());
+    });
+
+    
+    // Play a round of rock paper scissors
+    function playRound(playerSelection, computerSelection) {
+
+        // If tie restart round
+        if (playerSelection === computerSelection) {
+            results.innerHTML = "Tie. Repeat.";
+            return;
+        }
+
+        // Calculate and return the results
+        if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+            results.innerHTML = "Defeat! " + computerSelection + " beats " + playerSelection;
+            humanWins += 0;
+        }
+        if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+            results.innerHTML = "Victory! " + playerSelection + " beats " + computerSelection;
+            humanWins += 1;
+        }
+        if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+            results.innerHTML = "Victory! " + playerSelection + " beats " + computerSelection;
+            humanWins += 1;
+        }
+        if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+            results.innerHTML = "Defeat! " + computerSelection + " beats " + playerSelection;
+            humanWins += 0;
+        }
+        if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+            results.innerHTML = "Defeat! " + computerSelection + " beats " + playerSelection;
+            humanWins += 0;
+        }
+        if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+            results.innerHTML = "Victory! " + playerSelection + " beats " + computerSelection;
+            humanWins += 1;
+        }
+        currentRound += 1;
+        currentScore.innerHTML = "You: " + humanWins + ", opponent: " + Math.abs(humanWins - currentRound);
+
+        if (currentRound === totalRounds) {
+            if (humanWins > totalRounds / 2) {
+                results.innerHTML = "WINNER! You won " + humanWins + " of "+ totalRounds;
+            }
+            if (humanWins < totalRounds / 2) {
+                results.innerHTML = "LOSER! You won " + humanWins + " of "+ totalRounds;
+            }
+            // Disable buttons
+            btnRock.disabled = true;
+            btnPaper.disabled = true;
+            btnScissors.disabled = true;
+        }
     }
-    // Output overall winner
-    if (humanWins > rounds / 2) {
-        console.log("WINNER! You won " + humanWins + " of "+ rounds);
+
+    // Randomly return rock, paper, or scissors
+    function getComputerChoice() {
+        let choice = null;
+        let randNum = Math.floor(Math.random() * (4 - 1) + 1);
+        switch (randNum) {
+            case 1:
+                choice = "ROCK";
+                break;
+            case 2:
+                choice = "PAPER";
+                break;
+            case 3:
+                choice = "SCISSORS";
+                break;
+        }
+        return choice;
     }
-    if (humanWins < rounds / 2) {
-        console.log("LOSER! You won " + humanWins + " of "+ rounds);
-    }
-}
 
-// Play a round of rock paper scissors
-function playRound(playerSelection, computerSelection) {
-    // If tie restart round
-    while (playerSelection === computerSelection) {
-         console.log("Tie. Repeat.");
-         computerSelection = getComputerChoice();
-         console.log(computerSelection);
-         playerSelection = prompt("Rock, Paper, or Scissors?").toUpperCase();
-    }
-
-    // Calculate and return the results
-    if (playerSelection === "ROCK" && computerSelection === "PAPER") {
-        console.log("Defeat! " + computerSelection + " beats " + playerSelection);
-        return 0;
-    }
-    if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
-        console.log("Victory! " + playerSelection + " beats " + computerSelection);
-        return 1;
-    }
-    if (playerSelection === "PAPER" && computerSelection === "ROCK") {
-        console.log("Victory! " + playerSelection + " beats " + computerSelection);
-        return 1;
-    }
-    if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
-        console.log("Defeat! " + computerSelection + " beats " + playerSelection);
-        return 0;
-    }
-    if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
-        console.log("Defeat! " + computerSelection + " beats " + playerSelection);
-        return 0;
-    }
-    if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
-        console.log("Victory! " + playerSelection + " beats " + computerSelection);
-        return 1;
-    }
-}
-
-  // Randomly return rock, paper, or scissors
-  function getComputerChoice() {
-    let choice = null;
-    let randNum = Math.floor(Math.random() * (4 - 1) + 1);
-    switch (randNum) {
-        case 1:
-            choice = "ROCK";
-            break;
-        case 2:
-            choice = "PAPER";
-            break;
-        case 3:
-            choice = "SCISSORS";
-            break;
-    }
-    return choice;
-  }
-
-// Play n number of games
-game(5);
-
-
-/** 
-
-Pseudocode
-
-SET playerSelection TO NULL
-
-WHILE playerSelection IS NOT equal to rock, paper, or scissors
-    SET playerSelection TO Get user input
-
-SET randNum TO GET random number from 1 to 3
-
-SWITCH (randNum) {
-    case 1:
-        SET computerSelection TO rock
-    case 2:
-        SET computerSelection TO paper
-    case 3:
-        SET computerSelection TO scissors
-}
-
-FUNCTION playRound(playerSelection, computerSelection){
-    IF playerSelection === computerSelection repeat
-    ELSE 
-        IF playerSelection === rock AND computerSelection === paper RETURN victory
-        IF playerSelection === rock AND computerSelection === scissors RETURN defeat
-        IF playerSelection === paper AND computerSelection === rock RETURN victory
-        IF playerSelection === paper AND computerSelection === scissors RETURN defeat
-        IF playerSelection === scissors AND computerSelection === rock RETURN defeat
-        IF playerSelection === scissors AND computerSelection === paper RETURN victory
-}
-
-
-**/ 
+}); 
